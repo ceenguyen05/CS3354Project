5bfb7ed3a0b9778074abdac12c939a3a4bcd7a5b
k_pulik <63879539+kpulik@users.noreply.github.com>
2025-04-08 09:56:37 -0500

    Added files to integrate into backend code and updated gitignore file to not upload serviceAccountKey.json file

---
+++ Front end code to integrate/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart'; // Import the HomeScreen
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Disaster Relief Web App',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: const HomeScreen(), // Set HomeScreen as the starting point
+    );
+  }
+}
+
---
+++ Front end code to integrate/lib/models/user.dart
@@ -0,0 +1,23 @@
+// getting info from json file 
+class User {
+  final String email;
+  final String password;
+
+  User({required this.email, required this.password});
+
+  // Convert User to JSON format
+  Map<String, dynamic> toJson() {
+    return {
+      'email': email,
+      'password': password,
+    };
+  }
+
+  // Convert JSON to User
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      email: json['email'],
+      password: json['password'],
+    );
+  }
+}
