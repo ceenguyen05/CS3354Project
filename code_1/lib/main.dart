7b1e0d3062f2116ea0aa6d6b6df90488890f46ec
Casey Nguyen <cxn220034@utdallas.edu>
2025-04-24 13:24:15 -0500

    added stripe checkout

--- code_1/lib/main.dart
+++ code_1/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Disaster Relief Web App',
+      title: 'Disaster Relief Platform',
       theme: ThemeData(
         primarySwatch: Colors.indigo,
         fontFamily: 'ComicSans', 
